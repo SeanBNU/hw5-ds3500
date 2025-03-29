972d09a9eca62f7706c93ac0a97ebc25e7bb7131
Shourya Yadav <shourya0523@gmail.com>
2025-03-28 23:06:42 -0400

    new files

--- sorting.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-sections_path='data/sections.csv'
-tas_path='data/tas.csv'
-#ta_cols = ["max_assigned"] + [str(i) for i in range(17)]
-tas = (pd.read_csv(tas_path)).iloc[:,3:]
-print((pd.read_csv('data/test1.csv', header=None)).values.shape)
