a529f62a840e917054488dcc7ae35627751dae93
Sean Blundin <140537694+SeanBNU@users.noreply.github.com>
2025-03-29 02:13:26 -0400

    delete

--- SAMPLE_data/sample_maker.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-file_path = 'data/'
-file_list = ["sections.csv", "tas.csv", "test1.csv", "test2.csv", "test3.csv"]
-for file in file_list:
-    df = pd.read_csv(os.path.join(file_path, file))
-    df.head(10).to_csv(f"sample_{file}", index=False)
-    print(f"Saved sample_{file}")
